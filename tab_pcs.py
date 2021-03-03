acde114437e0cc043be8d9d1bb3f4b86a7478292
Fuhito Suguri <bitwalk@gmail.com>
2021-03-03 09:59:48 +0900

    update

--- tab_pcs.py
+++ tab_pcs.py
@@ -3,7 +3,11 @@
 
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import (
+    QLabel,
+    QGridLayout,
+    QSizePolicy,
     QTabWidget,
+    QWidget,
 )
 from database import SqlDB
 from win_db_basic import WinDBData
@@ -15,8 +19,21 @@ from resource import Icons
 # =============================================================================
 #  DBTab - Tab related to Database
 # =============================================================================
-class TabPCS(QTabWidget):
+class TabPCS(QWidget):
     def __init__(self, db: SqlDB):
         super().__init__()
         self.db = db
         self.icons = Icons()
+
+        #self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        grid = QGridLayout()
+        self.setLayout(grid)
+        row = 0
+
+        # ---------------------------------------------------------------------
+        # SUPPLIER dump
+        lab_supplier = QLabel('Supplier')
+        lab_supplier.setStyleSheet("QLabel {font-size:10pt; padding: 0 2px;}")
+        #lab_supplier.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        grid.addWidget(lab_supplier, row, 0)
+        row += 1
